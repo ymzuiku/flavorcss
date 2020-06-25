de22c59eeea4c98b20765d2918f1a7c9d80a8efd
梁柱 <ymblender@gmail.com>
2020-06-26 00:21:02 +0800

    feat: change title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function MyApp({ Component, pageProps }: any) {
 
   return (
     <>
-      <title>My page title</title>
+      <title>Flavorcss</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui, viewport-fit=cover"
