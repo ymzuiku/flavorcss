6a8de48284a6aac99d44cbe2b9fa10b85b715ffd
pillar <ymblender@gamil.com>
2021-02-01 18:29:46 +0800

    feat: single group

--- lib/classGroup.ts
+++ lib/classGroup.ts
@@ -21,25 +21,26 @@ export const classGroup = (group: any, name: string, value: string) => {
     });
     let out = "";
     css.split(" ").forEach((v) => {
+      // 暂时不支持组件嵌套组件
       // 若css中还有其他 comp，则递归查找，拼接到 out 中
       // 兼容组件名称中带有参数
-      const fix = fixClassName(group, v);
-      if (fix.comp as any) {
-        const _list = [] as string[];
-        v.split(":").forEach((_v) => {
-          if (_v === fix.compName) {
-            _list.push(fix.comp(fix.value.split(",")));
-          } else if (_v === fix.value) {
-          } else {
-            _list.push(_v);
-          }
-        });
-        // console.log(_list, "2222");
-        out += _list.join(":") + " ";
-      } else {
-        out += v + " ";
-      }
-      // out += (comp ? comp(value.split(",")) : v) + " ";
+      // const fix = fixClassName(group, v);
+      // if (fix.comp as any) {
+      //   const _list = [] as string[];
+      //   v.split(":").forEach((_v) => {
+      //     if (_v === fix.compName) {
+      //       _list.push(fix.comp(fix.value.split(",")));
+      //     } else if (_v === fix.value) {
+      //     } else {
+      //       _list.push(_v);
+      //     }
+      //   });
+      //   // console.log(_list, "2222");
+      //   out += _list.join(":") + " ";
+      // } else {
+      //   out += v + " ";
+      // }
+      out += v + " ";
     });
     console.log(out);
     return out;
