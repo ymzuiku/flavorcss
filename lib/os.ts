44f99e5ba9f6507da87ddb3122da997f6633fce0
pillar <ymblender@gamil.com>
2021-04-23 16:53:50 +0800

    fix: 精简base-css

--- lib/os.ts
+++ lib/os.ts
@@ -1,18 +1,3 @@
-// let _device: any;
-// export interface Device {
-//   android: boolean;
-//   firefox: boolean;
-//   chrome: boolean;
-//   tablet: boolean;
-//   ios: boolean;
-//   wechat: boolean;
-//   desktop: boolean;
-//   iphone: boolean;
-//   phone: boolean;
-//   safari: boolean;
-//   isNeedSafeTop: boolean;
-//   isNeedSafeBottom: boolean;
-// }
 const ua = navigator.userAgent.toLocaleLowerCase();
 
 const android = /(?:android)/.test(ua);
@@ -28,14 +13,10 @@ const ios = /(?:iphone)/.test(ua) && !tablet;
 const desktop = !ios && !android;
 const phone = !desktop;
 const wechat = phone && /(micromessenger|wechat)/.test(ua);
-const isNeedSafeBottom = ios && window.screen.height >= 812;
-const isNeedSafeTop = window.location.href.indexOf("native") > -1;
 
 // 获取是否是 ios 或 android
 
 export const os = {
-  isNeedSafeBottom,
-  isNeedSafeTop,
   android,
   firefox,
   chrome,
