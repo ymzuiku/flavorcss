c066d3904edd3b61ecf26120a846c81fc5080903
pillar <ymblender@gmail.com>
2021-07-24 13:49:56 +0800

    fix: add ICP

--- flavorcss-website/src/main.tsx
+++ flavorcss-website/src/main.tsx
@@ -1,9 +1,15 @@
 import "aoife";
 import { App } from "./app";
 import { Route } from "vanilla-route";
+import { ICP } from "./components/ICP";
 
+document.body.hidden = true;
 document.body.append(
   Route({ url: "/", render: App }),
   Route({ url: "/doc", render: () => import("./doc") }),
-  Route({ url: "/example", render: () => import("./example") })
+  Route({ url: "/example", render: () => import("./example") }),
+  ICP(),
 );
+setTimeout(()=>{
+  document.body.hidden = false;
+}, 100)
