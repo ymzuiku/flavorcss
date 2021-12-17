835c08d95ecb228320f7fd6cf7fb1f030773fd38
pillar <ymblender@gmail.com>
2021-12-18 06:06:51 +0800

    fix: base use cjs

--- lib/index.ts
+++
@@ -1,8 +0,0 @@
-import { obserer } from "./observer";
-import { parseClass } from "./parseClass";
-
-// eslint-disable-next-line
-(window as any).flavorcss = obserer;
-(window as any).flavorParse = parseClass;
-
-export { obserer, parseClass };
