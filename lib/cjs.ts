c6d62c5d10d59ee98424ad9dbd9bb90714fa8400
pillar <ymblender@gmail.com>
2021-12-18 06:37:58 +0800

    fix: cjs expor

--- lib/cjs.ts
+++ lib/cjs.ts
@@ -4,3 +4,7 @@ import { parseClass } from "./parseClass";
 // eslint-disable-next-line
 (window as any).flavorcss = obserer;
 (window as any).flavorParse = parseClass;
+module.exports = {
+  obserer,
+  parseClass,
+};
